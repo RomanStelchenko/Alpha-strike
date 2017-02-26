b3a16aeeab3565184e6db64d239c5079551d3544
GordiyevichSergey <gardi.sergey@gmail.com>
2017-02-26 16:14:51 +0300

    Button for shots

---
+++ Game/app/src/main/java/com/example/raduga/alphastrike/ShotView.java
@@ -0,0 +1,95 @@
+package com.example.raduga.alphastrike;
+
+import android.content.Context;
+import android.graphics.*;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+/**
+ * Created by gardi on 26.02.2017.
+ */
+public class ShotView extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener{
+
+    private float centerX, centerY;
+    private float radius;
+
+    public ShotView(Context context) {
+        super(context);
+        this.setBackgroundColor(Color.TRANSPARENT);
+        this.setZOrderOnTop(true);
+        getHolder().setFormat(PixelFormat.TRANSPARENT);
+        getHolder().addCallback(this);
+        setOnTouchListener(this);
+    }
+
+    public ShotView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        this.setBackgroundColor(Color.TRANSPARENT);
+        this.setZOrderOnTop(true);
+        getHolder().setFormat(PixelFormat.TRANSPARENT);
+        getHolder().addCallback(this);
+        setOnTouchListener(this);
+    }
+
+    public ShotView(Context context, AttributeSet attributeSet, int style) {
+        super(context, attributeSet, style);
+        this.setBackgroundColor(Color.TRANSPARENT);
+        this.setZOrderOnTop(true);
+        getHolder().setFormat(PixelFormat.TRANSPARENT);
+        getHolder().addCallback(this);
+        setOnTouchListener(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        setupDimensions();
+        drawView();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    private void setupDimensions() {
+        centerX = getWidth() / 2;
+        centerY = getHeight() / 2;
+        radius = Math.min(getWidth(), getHeight()) / 3;
+    }
+
+    public void drawView() {
+        if(this.getHolder().getSurface().isValid()) {
+            Canvas canvas = this.getHolder().lockCanvas();
+            Paint colors  = new Paint();
+            canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+            colors.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(centerX, centerY, radius, colors);
+            colors.reset();
+            colors.setARGB(255, 212, 36 ,54);//red
+            canvas.drawLine(centerX, centerY + radius/8, centerX, centerY + (radius*7/8), colors);
+            canvas.drawLine(centerX + radius/8, centerY, centerX + (radius*7/8), centerY, colors);
+            canvas.drawLine(centerX, centerY - radius/8, centerX, centerY - (radius*7/8), colors);
+            canvas.drawLine(centerX - radius/8, centerY, centerX - (radius*7/8), centerY, colors);
+            getHolder().unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public boolean onTouch(View v, MotionEvent e) {
+
+        if(v.equals(this)) {
+            if(e.getAction() == e.ACTION_DOWN) {
+
+            }
+        }
+
+        return true;
+    }
+}
